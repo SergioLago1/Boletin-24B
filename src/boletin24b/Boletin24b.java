0f0e954d30e71aa723969373ed64b5cd9a0d3c11
julianMGCODE <julianmosquerag@gmail.com>
2016-04-07 11:43:28 +0200

    FIN

---
+++ src/boletin24b/Boletin24b.java
@@ -0,0 +1,16 @@
+
+package boletin24b;
+
+/**
+ *
+ * @author julian
+ */
+public class Boletin24b {
+
+   
+    public static void main(String[] args) {
+       Interfaz i= new Interfaz();
+       i.setVisible(true);
+    }
+    
+}
